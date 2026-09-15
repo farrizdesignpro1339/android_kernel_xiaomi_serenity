3b736972b4a9d7dcfdbcc46923e9068f6c89a828
garenafreefiremax1339-ctrl <garenafreefiremax1339@gmail.com>
2026-09-15 18:57:20 +0700

    Initial commit

--- drivers/mmc/host/sdhci-sprd-ffu.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _SDHCI_SPRD_FFU_H
-#define _SDHCI_SPRD_FFU_H
-
-int mmc_ffu_init(struct sdhci_host *host);
-void mmc_ffu_remove(struct sdhci_host *host);
-
-#endif
-
--- include/linux/mfd/sc27xx-pmic.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __LINUX_MFD_SC27XX_PMIC_H
-#define __LINUX_MFD_SC27XX_PMIC_H
-
-extern enum usb_charger_type sprd_pmic_detect_charger_type(struct device *dev);
-extern int sprd_pmic_power_boot_time(u32 *count);
-
-#endif /* __LINUX_MFD_SC27XX_PMIC_H */
